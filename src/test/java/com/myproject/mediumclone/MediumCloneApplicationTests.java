2bcdffe738212affbecf82edaaaf7686696ccfac
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2021-08-13 19:21:29 +0530

    first commit

---
+++ src/test/java/com/myproject/mediumclone/MediumCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.mediumclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
